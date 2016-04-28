a7d8c869a40448df21ca7ac07eba5735bceb4035
Batchkill <batchkill@gmail.com>
2016-04-28 06:12:31 -0700

    Update pcap2csv.sh

--- pcap2csv.sh
+++ pcap2csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This script converts pcap files to a csv file using tshark
-
+# You will need tshark installed on your box
 
 for f in *.pcap
 do tshark –r "$f" -T fields -e frame.time -e tcp.stream -e ip.src -e ip.dst -e _ws.col.Protocol -e tcp.srcport -e tcp.dstport -e tcp.len -e tcp.window_size -e tcp.flags.syn -e tcp.flags.ack -e tcp.flags.push -e tcp.flags.fin -e tcp.flags.reset -e ip.ttl -e _ws.col.Info -e tcp.analysis.ack_rtt -e vlan.id > "$f".csv
